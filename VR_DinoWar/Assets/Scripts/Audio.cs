88cca1b0615123ccb7a17721994f7354333dd05b
PC-27\user <ptanh2212@gmail.com>
2018-04-05 10:33:22 +0700

    2/4/2018

---
+++ VR_DinoWar/Assets/Scripts/Audio.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : Cacheable {
+	
+	public AudioSource audioSource ;
+
+	public override void OnLive ()
+	{
+		audioSource.Play();
+	}
+
+	public override void OnDestroy ()
+	{
+		
+	}
+		
+		
+	// Use this for initialization
+	void Start () {
+		Destroy ();
+	}
+
+	float timeCount = 0;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!_living)
+			return;
+
+		timeCount += Time.deltaTime;
+
+		if (timeCount > audioSource.clip.length) {
+			timeCount = 0;
+			Destroy ();
+		}
+
+	}
+}
---
+++ VR_DinoWar/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour 
+{
+	public static AudioManager instance;
+
+	private Dictionary<SoundFX,AudioClip> clips;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	IEnumerator Start()
+	{
+		clips = new Dictionary<SoundFX, AudioClip> {
+
+			{ SoundFX.None, null },
+			{ SoundFX.AmbienceBird, Resources.Load<AudioClip>("Sounds/Effects/AmbienceBird") },
+			{ SoundFX.AmbienceRiver, Resources.Load<AudioClip>("Sounds/Effects/AmbienceRiver") },
+			{ SoundFX.AmbienceWind, Resources.Load<AudioClip>("Sounds/Effects/AmbienceWind") },
+			{ SoundFX.ArrowFly, Resources.Load<AudioClip>("Sounds/Effects/ArrowFly") },
+			{ SoundFX.FatDinoAttack1, Resources.Load<AudioClip>("Sounds/Effects/FatDinoAttack1") },
+			{ SoundFX.FatDinoAttack2, Resources.Load<AudioClip>("Sounds/Effects/FatDinoAttack2") },
+			{ SoundFX.FatDinoDie1, Resources.Load<AudioClip>("Sounds/Effects/FatDinoDie1") },
+			{ SoundFX.FatDinoDie2, Resources.Load<AudioClip>("Sounds/Effects/FatDinoDie2") },
+			{ SoundFX.FatDinoHit1, Resources.Load<AudioClip>("Sounds/Effects/FatDinoHit1") },
+			{ SoundFX.FatDinoHit2, Resources.Load<AudioClip>("Sounds/Effects/FatDinoHit2") },
+			{ SoundFX.FatDinoPee, Resources.Load<AudioClip>("Sounds/Effects/FatDinoPee") },
+			{ SoundFX.Impact, Resources.Load<AudioClip>("Sounds/Effects/Impact") },
+			{ SoundFX.Whoosh, Resources.Load<AudioClip>("Sounds/Effects/Whoosh") },
+			{ SoundFX.LongbowPullBack01, Resources.Load<AudioClip>("Sounds/Effects/LongbowPullBack01") },
+			//{ SoundFX.Whoosh, Resources.Load<AudioClip>("Sounds/Effects/Whoosh") },
+		};
+
+		yield return new WaitForSeconds (1);
+	}
+
+	public enum SoundFX
+	{
+		None,
+		AmbienceBird,
+		AmbienceRiver,
+		AmbienceWind,
+		ArrowFly,
+		FatDinoAttack1,
+		FatDinoAttack2,
+		FatDinoConfused,
+		FatDinoDie1,
+		FatDinoDie2,
+		FatDinoHit1,
+		FatDinoHit2,
+		FatDinoPee,
+		Impact,
+		Whoosh,
+		LongbowPullBack01,
+	}
+
+	public void PlayClip(SoundFX soundFX,Vector3 worldPos,float volume = 1)
+	{
+		AudioClip clip;
+
+		if (clips.TryGetValue (soundFX, out clip)) {
+			Audio audio = ObjectPool.instance.GetAudioSoure ();
+			audio.audioSource.clip = clip;
+			audio.audioSource.volume = volume;
+			audio.transform.position = worldPos;
+			audio.Live ();
+		}
+	}
+
+
+}
